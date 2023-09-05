6532b23a1f5840342937b297abdacdbe86b1875e
jtiepmar <root@D5D0B82>
2023-09-05 12:25:59 +0000

    request for dates

---
+++ plain/dates.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: text/plain');
+require('../config.php');
+
+function urncount(){
+	global $sql;
+	$query = "SELECT year, count(year) as cd FROM workdata GROUP BY year ORDER BY year";
+	$res = "";
+	foreach ($sql->query($query) as $row) {
+		$res = $res.$row['year']."\t".$row['cd']."\n";
+	}
+	return $res;
+}
+echo urncount();
+?>
