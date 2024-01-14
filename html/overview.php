55e29e24e083bb3d6a0d4aa9a067cb21d8b9d850
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-01-14 13:11:04 +0100

    overview w offset

--- html/overview.php
+++ html/overview.php
@@ -6,10 +6,14 @@ require('../config.php');
 
 function editions(){
 	global $sql;
-	$query = "SELECT urn,title,year,author FROM workdata";
-	$res = "<table><tr><th>URN</th><th>Passage</th><th>Structure</th><th>Label</th><th>Date</th>";
+	$offset = 0;
+	if (isset($_GET['offset'])){
+		$offset = $_GET['offset'];
+	}
+	$query = "SELECT urn,title,year,author FROM workdata ORDER BY urn LIMIT 5000 OFFSET ".$offset;
+	$res = "<table>";
 	foreach ($sql->query($query) as $row) {
-		$res = $res.'<tr><td>'.$row['urn'].'</td><td>'.'<a href="../plain/passage.php?urn='.$row['urn'].'&deletexml&nl">Passage</a>'.'</td><td>'.'<a href="../html/structure.php?urn='.$row['urn'].'">Structure</a>'.'</td><td>'.$row['title'].'</td><td>'.$row['year'].'</td><td>'.$row['author']."</td></tr>";
+		$res = $res.'<tr><td><a href="../plain/passage.php?urn='.$row['urn'].'&deletexml&nl">Passage</a></td><td><a href="../html/structure.php?urn='.$row['urn'].'">Structure</a></td><td>'.$row['urn'].'</td><td>'.$row['title'].'</td><td>'.$row['year'].'</td><td>'.$row['author']."</td></tr>";
 	}
 	$res = $res."</table>";
 	return $res;
