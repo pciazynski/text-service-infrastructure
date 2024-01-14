2d80bd657a0015a66eac5fb8603302ab758aa4da
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-01-14 12:42:06 +0100

    offset for large TI

--- html/editions.php
+++ html/editions.php
@@ -6,7 +6,11 @@ require('../config.php');
 
 function editions(){
 	global $sql;
-	$query = "SELECT urn,title,year,author FROM workdata";
+	$offset = 0;
+	if (isset($_GET['offset'])){
+		$offset = $_GET['offset'];
+	}
+	$query = "SELECT urn,title,year,author FROM workdata ORDER BY urn LIMIT 25000 OFFSET ".$offset;
 	$res = "";
 	foreach ($sql->query($query) as $row) {
 		$res = $res.'<a href="../plain/passage.php?urn='.$row['urn'].'&deletexml&nl">'.$row['urn'].'</a><br/>';
--- plain/editions.php
+++ plain/editions.php
@@ -5,7 +5,11 @@ require('../config.php');
 
 function editions(){
 	global $sql;
-	$query = "SELECT urn,title,year,author FROM workdata";
+	$offset = 0;
+	if (isset($_GET['offset'])){
+		$offset = $_GET['offset'];
+	}
+	$query = "SELECT urn,title,year,author FROM workdata ORDER BY urn LIMIT 25000 OFFSET ".$offset;
 	$res = "";
 	foreach ($sql->query($query) as $row) {
 		$res = $res.$row['urn']."\t".$row['title']."\t".$row['year']."\t".$row['author']."\n";
