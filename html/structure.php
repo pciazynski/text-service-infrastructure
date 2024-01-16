b560a1fbab7088d74669b49f993f323a6f3848e4
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-01-16 13:02:32 +0100

    more basic html strct

--- html/structure.php
+++ html/structure.php
@@ -9,11 +9,10 @@ function getDocStrct($urn,$isWorkurn){
 	if($isWorkurn){$query = "SELECT urn,type,CHAR_LENGTH(text) AS len FROM urndata WHERE urn LIKE '".$urn."%' ORDER BY urnid";}
 	else {$query = "SELECT urn,type,CHAR_LENGTH(text)  AS len FROM urndata WHERE urn LIKE '".$urn.".%' ORDER BY urnid";}
 
-	$res = "<table><tr><th>URN</th><th>Type</th><th>Text length incl. XML</th>";
+	$res = "";
 	foreach ($sql->query($query) as $row) {
-		$res = $res.'<tr><td><a href="../plain/passage.php?urn='.$row['urn'].'&deletexml&nl">'.$row['urn'].'</a>'."</td><td>".$row['type']."</td><td>".$row['len']."</td></tr>";
+		$res = $res.'<a href="../plain/passage.php?urn='.$row['urn'].'&deletexml&nl">'.$row['urn'].'</a>'." ".$row['type']." ".$row['len']."<br/>";
 	}
-	$res = $res."</table>";
 
 	return trim($res);
 }
@@ -22,7 +21,7 @@ $urn = trim(htmlspecialchars($_GET["urn"]));
 $urnarr = checkurn($urn);
 if ($urnarr !== false){
 	if (strlen($urnarr[4]) == 0){echo getDocStrct($urn,true);}
-	elseif (strpos ($urnarr[4],"-")){echo getSpanningPassage($urn);}
+	elseif (strpos ($urnarr[4],"-")){echo "";}
 	else {echo getDocStrct($urn,false);};
 }
 
