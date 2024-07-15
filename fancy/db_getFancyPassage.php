e04ddc97f179923aee5d0a4185f203e07d949509
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-07-15 10:32:31 +0200

    ebook

--- fancy/db_getFancyPassage.php
+++ fancy/db_getFancyPassage.php
@@ -5,14 +5,26 @@ function passage($urn){
 	global $binary;
 	
 	#LIKE urn.% OR (exactly) LIKE urn
-	if(str_ends_with($urn,":")){$query = "SELECT text,type FROM urndata WHERE urn LIKE ".$binary." '".$urn."%' ORDER BY urnid";}
-	else {$query = "SELECT text,type FROM urndata WHERE urn LIKE ".$binary." '".$urn.".%' OR urn LIKE ".$binary." '".$urn."' ORDER BY urnid";}
+	if(str_ends_with($urn,":")){$query = "SELECT text,type,urn FROM urndata WHERE urn LIKE ".$binary." '".$urn."%' ORDER BY urnid LIMIT 1,18446744073709551615";}
+	else {$query = "SELECT text,type,urn FROM urndata WHERE urn LIKE ".$binary." '".$urn.".%' OR urn LIKE ".$binary." '".$urn."' ORDER BY urnid";}
 	$res = "";
-	foreach ($sql->query($query) as $row) {
-		$res = $res.'<'.$row['type'].'>'.preg_replace('/<[^>]+>/', "", $row['text']).'</'.$row['type'].'>';
-	}
-	
+	$stack = [];
+	$oldurnpartcount = 0;
 
+	foreach ($sql->query($query) as $row) {
+		$newurnpartcount = count(explode(".",explode(":",$row['urn'])[4]));
+		if ($newurnpartcount==1){
+			$res = $res.'<hr>';
+		}
+		
+		while ($newurnpartcount<=count($stack)){
+			$res = $res.'</'.array_pop($stack).'>';
+		}
+		$type = str_replace("head","h".$newurnpartcount,$row['type']);
+		$res = $res.'<'.$type.'>';
+		array_push($stack,$type);
+		$res = $res.preg_replace('/<[^>]+>/', "", $row['text']);
+	}
 	return $res;
 }
 ?>
---
+++ fancy/ebook.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: text/html');
+require('../functions.php');
+require('../config.php');
+require('db_getFancyPassage.php');
+
+function ebookeditions(){
+	global $sql;
+	$offset = 0;
+	if (isset($_GET['offset'])){
+		$offset = $_GET['offset'];
+	}
+	$query = "SELECT urn,title,year,author FROM workdata ORDER BY urn LIMIT 10000 OFFSET ".$offset;
+	$res = "<ul>";
+	$tab = "\t";
+	$nl = "\n";
+	foreach ($sql->query($query) as $row) {
+		$res .= '<li><a href="?urn='.$row['urn'].'">';
+		$res .= $row['title'];
+		$res .= '</a></li>';
+	}
+	$res = $res."</ul>";
+	return $res;
+}
+
+
+if (isset($_GET["urn"])){
+	$urn = trim(htmlspecialchars($_GET["urn"]));
+	$html = '<!DOCTYPE html><link rel="stylesheet" href="ebook.css">';
+	$html.=str_replace("\n","<br>",passage($urn));
+	
+	echo $html;
+}else{
+	echo ebookeditions();
+}
+?>
