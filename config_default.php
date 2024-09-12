612c478141677ebe7f0505d38657e7112b7d570e
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-09-12 10:21:48 +0200

    move punct config

--- config_default.php
+++ config_default.php
@@ -4,4 +4,12 @@ require('constants.php');
 #SQL information
 $sql = new PDO('mysql:host=localhost;dbname=cts', 'ctsapi', 'pw');
 $multibyte = false;
+
+#normalization for tm. not relevant standard usage.
+$replacearr = array("}","{","♦","†","‚","‘","‘","_","«","»","―","•","☚","☛","+","|","*","#","'","}","=","/","!","”","…",'"',",",".","?","(",")","[","]",";",":","—","“","„");
+$punctarr = array(",",".","!","?","―");
+
+#change this to "BINARY" if you want case sensitive URNs at the cost of performance
+$binary = "";
+	
 ?>
--- constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-	#normalization for tm. not relevant standard usage.
-	$replacearr = array("}","{","♦","†","’","‚","‘","‘","_","«","»","―","•","☚","☛","+","|","*","#","'","}","=","/","!","”","…",'"',"–",",",".","?","(",")","[","]",";",":","—","“","-","„");
-	$punctarr = array(",",".","!","?","―");
-
-	#change this to "BINARY" if you want case sensitive URNs at the cost of performance
-	$binary = "";
-?>
