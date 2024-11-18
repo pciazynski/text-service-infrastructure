f9a21b2f4bd0908686e1b043a80c18d6ab4d6ecd
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-11-18 11:56:10 +0100

    edition filtering

--- plain/editions.php
+++ plain/editions.php
@@ -3,31 +3,36 @@ header('Content-Type: text/plain');
 require('../config.php');
 
 # Returns edition level URN list.
-# Params: none
+# Params: offset, urnfilter, author, title, year
 
 function editions(){
 	global $sql;
-	if (file_exists("editions.cache")){
-		return file_get_contents("editions.cache");
+	$offset = 0;
+	if (isset($_GET['offset'])){
+		$offset = $_GET['offset'];
 	}
-	else
-	{
-		$offset = 0;
-		if (isset($_GET['offset'])){
-			$offset = $_GET['offset'];
-		}
-		$query = "SELECT urn,title,year,author,restricted FROM workdata ORDER BY urn";
-		$res = "";
-		$tab = "\t";
-		$nl = "\n";
-		foreach ($sql->query($query) as $row) {
-			$res = $res.$row['urn'].$tab.$row['title'].$tab.$row['year'].$tab.$row['author'].$tab.$row['restricted'].$nl;
-		}
-		$cache = fopen("editions.cache", "w") or die("Unable to open file!");
-		fwrite($cache, $res);
-		fclose($cache);
-		return $res;
+	$condi = " WHERE TRUE";
+	if (isset($_GET['urnfilter'])){
+		$condi .= ' AND urn LIKE "%'.$_GET['urnfilter'].'%"';
 	}
+	if (isset($_GET['author'])){
+		$condi .= ' AND author LIKE "%'.$_GET['author'].'%"';
+	}
+	if (isset($_GET['title'])){
+		$condi .= ' AND title LIKE "%'.$_GET['title'].'%"';
+	}
+	if (isset($_GET['year'])){
+		$condi .= ' AND year '.$_GET['year'];
+	}
+
+	$query = "SELECT urn,title,year,author,restricted FROM workdata".$condi." ORDER BY urn";
+	$res = "";
+	$tab = "\t";
+	$nl = "\n";
+	foreach ($sql->query($query) as $row) {
+		$res = $res.$row['urn'].$tab.$row['title'].$tab.$row['year'].$tab.$row['author'].$tab.$row['restricted'].$nl;
+	}
+	return $res;
 }
 echo editions();
 ?>
