c1668eae9457a20e9ddb64a0da3ab77456aba992
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-11-18 13:59:01 +0100

    edition sorting

--- plain/editions.php
+++ plain/editions.php
@@ -8,24 +8,29 @@ require('../config.php');
 function editions(){
 	global $sql;
 	$offset = 0;
+	$sortBy = "urn";
+	
+	if (isset($_GET['sortBy']) and strlen(trim($_GET['sortBy']))>0){
+		$sortBy = $_GET['sortBy'];
+	}
 	if (isset($_GET['offset'])){
 		$offset = $_GET['offset'];
 	}
 	$condi = " WHERE TRUE";
-	if (isset($_GET['urnfilter'])){
+	if (isset($_GET['urnfilter']) and strlen(trim($_GET['urnfilter']))>0){
 		$condi .= ' AND urn LIKE "%'.$_GET['urnfilter'].'%"';
 	}
-	if (isset($_GET['author'])){
+	if (isset($_GET['author']) and strlen(trim($_GET['author']))>0){
 		$condi .= ' AND author LIKE "%'.$_GET['author'].'%"';
 	}
-	if (isset($_GET['title'])){
+	if (isset($_GET['title']) and strlen(trim($_GET['title']))>0){
 		$condi .= ' AND title LIKE "%'.$_GET['title'].'%"';
 	}
-	if (isset($_GET['year'])){
+	if (isset($_GET['year']) and strlen(trim($_GET['year']))>0){
 		$condi .= ' AND year '.$_GET['year'];
 	}
 
-	$query = "SELECT urn,title,year,author,restricted FROM workdata".$condi." ORDER BY urn LIMIT 10000 OFFSET ".$offset;
+	$query = "SELECT urn,title,year,author,restricted FROM workdata".$condi." ORDER BY ".$sortBy." LIMIT 10000 OFFSET ".$offset;
 	$res = "";
 	$tab = "\t";
 	$nl = "\n";
