cc05eae394dc01a7f54d766b5e3a6a36f7af701e
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-11-18 15:08:19 +0100

    ...

--- plain/editions.php
+++ plain/editions.php
@@ -12,13 +12,14 @@ function editions(){
 			return file_get_contents("editions.cache");
 		}
 		else{
-		$query = "SELECT urn,title,year,author FROM workdata ORDER BY author,title,urn";
+			$query = "SELECT urn,title,year,author,restricted FROM workdata ORDER BY author,title,urn";
 			$res = "";
 			$tab = "\t";
 			$nl = "\n";
 			foreach ($sql->query($query) as $row) {
-				$res = $res.$row['urn'].$tab.$row['title'].$tab.$row['year'].$tab.$row['author'].$nl;
-			}		$cache = fopen("editions.cache", "w") or die("Unable to open file!");
+				$res = $res.$row['urn'].$tab.$row['title'].$tab.$row['year'].$tab.$row['author'].$tab.$row['restricted'].$nl;
+			}
+			$cache = fopen("editions.cache", "w") or die("Unable to open file!");
 			fwrite($cache, $res);
 			fclose($cache);
 			return $res;
