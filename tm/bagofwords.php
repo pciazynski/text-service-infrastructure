9ee7017ad7b33afc40bdb587ca28c6927ce7bc76
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-11-25 14:18:03 +0100

    bagowwords bugfix

--- tm/bagofwords.php
+++ tm/bagofwords.php
@@ -17,7 +17,7 @@ $psg = "";
 
 $urnarr = checkurn($urn);
 if ($urnarr !== false){
-	if (strlen($urnarr[4]) == 0){$psg = passage($urn,true, $deleteXML=true);}
+	if (strlen($urnarr[4]) == 0){$psg = passage($urn,$deleteXML=true);}
 	elseif (strpos ($urnarr[4],"-")){$psg =  spanningPassage($urn, $deleteXML=true);}
 	else {$psg = passage($urn, false, $deleteXML=true);};
 }
