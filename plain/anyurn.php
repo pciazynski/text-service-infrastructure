9e9883330589d187087e1e08a3062b3d2fbfab16
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-01-08 12:42:17 +0100

    rework to anyurn

--- plain/anyurn.php
+++ plain/anyurn.php
@@ -6,13 +6,7 @@ require('../config.php');
 # Returns a random low level URN e.g. for testing.
 # Params: 
 
-if(isset($_GET['offset'])){
-	$offset = $_GET['offset'];
-}
-else{
-	$offset=1;
-}
-$query = "SELECT urn from urndata WHERE urnid BETWEEN ".$offset." AND ".($offset+10)." AND TEXT IS NOT NULL LIMIT 1";
+$query = "SELECT urn from urndata WHERE TEXT IS NOT NULL LIMIT 1";
 $res = '';
 $nl = "\n";
 foreach ($sql->query($query) as $row) {
