03c252dec5f99f9f43ea6d0b3d5940050384e840
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-02-17 15:40:03 +0100

    ...

---
+++ config_default.php
@@ -0,0 +1,18 @@
+<?php
+
+#SQL information
+$sql = new PDO('mysql:host=localhost;dbname=cts', 'ctsapi', 'pw');
+$multibyte = false;
+
+#normalization for tm. not relevant standard usage.
+$replacearr = array("}","{","♦","†","‚","‘","‘","_","«","»","•","☚","☛","+","|","*","#","'","}","=","/","!","”","…",'"',",",".","?","(",")","[","]",";",":","“","„");
+$punctarr = array(",",".","!","?","―");
+
+#change this to "BINARY" if you want case sensitive URNs at the cost of performance
+$binary = "";
+
+#change to true if restricted documents are included. If set to false, then unnecessary database checks are skipped and no restricted document content is served.
+$restricteddocuments = false;
+$copyrighttoken = "";
+
+?>
