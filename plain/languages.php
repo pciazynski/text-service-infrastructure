650baa2181dc50d7d7012cfbd868f732fd59b5a3
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-02-28 15:01:27 +0100

    added plain/lang

---
+++ plain/languages.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: text/plain');
+require('../config.php');
+
+# Returns distinct document languages and their number. 
+# Params: none
+function lang(){
+	global $sql;
+	$query = "SELECT lang, count(lang) as cd FROM workdata WHERE lang IS NOT NULL GROUP BY lang ORDER BY lang";
+	$res = "";
+	$tab = "\t";
+	$nl = "\n";
+	foreach ($sql->query($query) as $row) {
+		$res = $res.$row['lang'].$tab.$row['cd'].$nl;
+	}
+	return $res;
+}
+echo lang();
+?>
+?>
