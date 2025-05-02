a7c799fcdb9ee98ee220478f94b9328cb60bb1e5
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-05-02 09:46:33 +0200

    ..

--- plain/label.php
+++ plain/label.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/plain');
 require('../functions.php');
 require('../config.php');
-require('../db_GetLabel.php');
+require('../db_getLabel.php');
 
 # Returns the label for a specified URN. The result is based on db_GetLabel.php
 # Params: urn
