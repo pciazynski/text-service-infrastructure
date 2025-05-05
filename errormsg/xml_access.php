176576eb75f3ff76f9705183a4313320e4396242
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-05-05 09:21:54 +0200

    ..

--- errormsg/xml_access.php
+++ errormsg/xml_access.php
@@ -2,7 +2,7 @@
 Header('Content-type: text/xml');
 
 $msg = "<CTSError xmlns=\"http://relaxng.org/ns/structure/1.0\" xmlns:a=\"http://relaxng.org/ns/compatibility/annotations/1.0\" xmlns:cts=\"http://chs.harvard.edu/xmlns/cts\" xmlns:tei=\"http://www.tei-c.org/ns/1.0\">";
-$msg.="<message>Restricted Access:".$_GET['urn']."</message><code>7</code></CTSError>";
+$msg.="<message>Restricted Access</message><urn>".$_GET['urn']."</urn><code>7</code></CTSError>";
 echo ($msg);
 exit();
 ?>
