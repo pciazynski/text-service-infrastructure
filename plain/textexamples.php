3c0cf6f62ff6021bc8d0ce54dcd43dff409a593e
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-05-06 12:52:04 +0200

    ..

--- plain/textexamples.php
+++ plain/textexamples.php
@@ -6,11 +6,20 @@ require('../config.php');
 if ( isset($_GET["snippet"])){
 	(isset($_GET["limit"])) ? $limit = $_GET["limit"] : $limit = "100";
 	$token = trim(htmlspecialchars($_GET['token']));
-	$query = "SELECT text FROM urndata WHERE text LIKE '% ".$_GET["snippet"]." %' LIMIT ".$limit;
 	$res = "";
 	$nl = "\n";
+	$i = 0;
+	$query = "SELECT text FROM urndata WHERE text LIKE '% ".$_GET["snippet"]." %' LIMIT ".$limit;
 	foreach ($sql->query($query) as $row){
+		$i = $i +1;
 		$res = $res.$row['text'].$nl;
+	}
+	if($i<$limit){
+		$limit = $limit - $i;
+		$query = "SELECT text FROM urndatarestr WHERE text LIKE '% ".$_GET["snippet"]." %' LIMIT ".$limit;
+		foreach ($sql->query($query) as $row){
+			$res = $res.$row['text'].$nl;
+		}
 	}
 	(isset($_GET["deletexml"])) ? $res = deletexml($res) : NULL;
 	echo ($res);
