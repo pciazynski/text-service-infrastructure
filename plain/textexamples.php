e93771ed9533d4e51e68e54a706af29f1407b66b
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-05-06 12:54:16 +0200

    ..

--- plain/textexamples.php
+++ plain/textexamples.php
@@ -4,19 +4,20 @@ require('../functions.php');
 require('../config.php');
 
 if ( isset($_GET["snippet"])){
-	(isset($_GET["limit"])) ? $limit = $_GET["limit"] : $limit = "100";
+	(isset($_GET["limit"])) ? $limit = $_GET["limit"] : $limit = "10";
 	$token = trim(htmlspecialchars($_GET['token']));
 	$res = "";
 	$nl = "\n";
 	$i = 0;
 	$query = "SELECT text FROM urndata WHERE text LIKE '% ".$_GET["snippet"]." %' LIMIT ".$limit;
 	foreach ($sql->query($query) as $row){
-		$i = $i +1;
+		$i = $i + 1;
 		$res = $res.$row['text'].$nl;
 	}
+	$limit = 5;
 	if($i<$limit){
 		$limit = $limit - $i;
-		$query = "SELECT text FROM urndatarestr WHERE text LIKE '% ".$_GET["snippet"]." %' LIMIT ".$limit;
+		$query = "SELECT text FROM urndatarestr WHERE text LIKE '% ".$_GET["snippet"]." %' ORDER BY text LIMIT ".$limit;
 		foreach ($sql->query($query) as $row){
 			$res = $res.$row['text'].$nl;
 		}
