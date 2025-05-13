9fa25ed19a0bb90a28d32bcc4ad198c9016ce968
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-05-13 15:34:58 +0200

    ..

--- tm/ngrams.php
+++ tm/ngrams.php
@@ -42,11 +42,11 @@ if ($ngramgoalsize>=2 and (!$restricteddocuments or $ngramgoalsize<10)){
 		foreach ($psgarr as $token){
 			$token = trim($token);
 			if(strlen($token) >0){
-				$ngram.="-".$token;
-				$ngram = trim($ngram,"-");
+				$ngram.=" ".$token;
+				$ngram = trim($ngram," ");
 				if(count(explode("-",$ngram)) == $ngramgoalsize){
 					(array_key_exists($ngram, $rs)) ? $rs[$ngram] = $rs[$ngram]+1 : $rs[$ngram] = 1;
-					$ngram = explode("-",$ngram,2)[1];
+					$ngram = explode(" ",$ngram,2)[1];
 				}
 			}
 		}
