b1b05622581f84dfbac36b6834c19cdbd95a1d1a
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-11-07 10:53:19 +0100

    textdistribution -> characters

--- plain/textdistribution.php
+++ plain/textdistribution.php
@@ -3,7 +3,26 @@ header('Content-Type: text/plain');
 require('../functions.php');
 require('../config.php');
 
-if (isset($_GET["urn"]) && isset($_GET["snippet"])){
+if (isset($_GET["urn"]) && isset($_GET["character"])){
+	$urn = checkurn($_GET['urn'],'');
+	$snippet =  trim(htmlspecialchars($_GET['character']));
+	$query = "SELECT urn, text, text FROM urndata WHERE urn LIKE ".$binary." '".$urn."%' AND text IS NOT NULL ORDER BY urnid";
+	$res = "";
+	$tab = "\t";
+	$nl = "\n";
+	foreach ($sql->query($query) as $row){
+		$psg = deletexml($row['text']);
+		if(isset($_GET['lowercase'])){
+			$snippet = strtolower($snippet);
+			($multibyte) ? $psg = mb_strtolower($psg,'UTF-8') : $psg = strtolower($psg);
+		}
+		$occ = count(explode($snippet,$psg))-1;
+		$res = $res.$row['urn'].$tab.$occ.$nl;
+	}
+	echo ($res);
+}
+
+else if (isset($_GET["urn"]) && isset($_GET["snippet"])){
 	$urn = checkurn($_GET['urn'],'');
 	$snippet =  trim(htmlspecialchars($_GET['snippet']));
 	$query = "SELECT urn, text, text FROM urndata WHERE urn LIKE ".$binary." '".$urn."%' AND text IS NOT NULL ORDER BY urnid";
