0399113038acfd6ae41dd334944205ee79853a20
JTiepmar <jochen.tiepmar@serbski-institut.de>
2026-02-02 16:06:15 +0100

    editions rw

--- plain/editions.php
+++ plain/editions.php
@@ -28,38 +28,45 @@ function editions(){
 			$yearmin = 1;
 			$yearmax = 30000;
 		}
-		if (isset($_GET['restricted'])){
-			intval($_GET['restricted']) == 1 ? $restricted = ' AND restricted = 1' : $restricted = ' AND restricted = 0';
-		}
-		else{
-			$restricted = '';
-		}
+		$query = 'SELECT urn,title,year,author,restricted,lang FROM workdata WHERE urn LIKE ? ';
+	
 		(isset($_GET['yearmin'])) ? $yearmin = $_GET['yearmin'] : $yearmin = 1;;
 		(isset($_GET['yearmax'])) ? $yearmax = $_GET['yearmax'] : $yearmax = 30000;;
-		(isset($_GET['urnfilter'])) ? $urnfilter = '%'.$_GET['urnfilter'].'%' : $urnfilter = '%';
-		(isset($_GET['lang'])) ? $lang = $_GET['urnfilter'] : $lang = '%';
-		(isset($_GET['author'])) ? $author = '%'.$_GET['author'].'%' : $author = '%';
-		(isset($_GET['title'])) ? $title = '%'.$_GET['title'].'%' : $title = '%';
+		(isset($_GET['urnfilter'])) ? $params=array($_GET['urnfilter']): $params=array('%');
 
-		$sortBy = 'urn';
+		if (isset($_GET['restricted'])){
+			intval($_GET['restricted']) == 1 ? $query .= ' AND restricted = 1' : $query = ' AND restricted = 0';
+		}
+		if(isset($_GET['lang'])){
+			$query.= ' AND lang LIKE ?';
+			array_push($params,$_GET['lang']);
+		}
+		if(isset($_GET['author'])){
+			$query.= ' AND author LIKE ?';
+			array_push($params,'%'.$_GET['author'].'%');
+		}
+		if(isset($_GET['title'])){
+			$query.= ' AND title LIKE ?';
+			array_push($params,'%'.$_GET['title'].'%');
+		}
+
 		if(isset($_GET['sortBy'])){
 			$tmp = $_GET['sortBy'];
 			switch($tmp){
 				case 'author':
-					$sortBy = 'author';
+					$query .= ' ORDER BY author';
 					break;
 				case 'year':
-					$sortBy = 'year';
+					$query .= ' ORDER BY year';
 					break;
 				case 'author,year':
-					$sortBy = 'author,year';
+					$query .= ' ORDER BY author,year';
 					break;
 			}
 		}
 		
-		
-		$stmt = $sql->prepare('SELECT urn,title,year,author,restricted,lang FROM workdata WHERE urn LIKE ? '.$restricted.' AND author LIKE ? and title LIKE ? and lang LIKE ? ORDER BY '.$sortBy);
-		$stmt->execute([$urnfilter, $author, $title, $lang]);
+		$stmt = $sql->prepare($query);
+		$stmt->execute($params);
 	}
 	foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
 		$res = $res.$row['urn'].$tab.$row['title'].$tab.$row['year'].$tab.$row['author'].$tab.$row['restricted'].$tab.$row['lang'].$nl;
