39db70b955e38277364a59beef3e544b503166d0
JTiepmar <jochen.tiepmar@serbski-institut.de>
2026-02-05 11:05:25 +0100

    ..

--- plain/searchtitles.php
+++ plain/searchtitles.php
@@ -28,7 +28,7 @@ function titles($snippet){
 	return $res;
 }
 
-if (isset($_GET['snippet'])){
+if (isset($_GET['snippet']) and strlen($_GET['snippet'])>3){
 	echo trim(titles($_GET['snippet']),"\n");
 }
 ?>
