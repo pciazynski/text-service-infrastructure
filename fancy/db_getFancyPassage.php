5593a370d1e2d1cb68039dd2bac05af3fe488fa3
JTiepmar <jochen.tiepmar@serbski-institut.de>
2026-03-06 11:23:04 +0100

    ..

--- fancy/db_getFancyPassage.php
+++ fancy/db_getFancyPassage.php
@@ -34,7 +34,6 @@ function spanningPassage($urn){
 	$fromurnid = getLeftOrRightUrnID($workurn.':'.$psgurn[0], true);
 	$tournid = getLeftOrRightUrnID($workurn.':'.$psgurn[1],false);
 	$res = '';
-	$stack = [];
 	$nl = "\n";
 	$stmt = $sql->prepare('SELECT text,type,urn FROM urndata WHERE urnid BETWEEN ? AND ? ORDER BY urnid');
 	$stmt->execute([$fromurnid,$tournid]);
@@ -44,57 +43,16 @@ function spanningPassage($urn){
 			$res = $res.'<hr>';
 		}
 		
-		while ($newurnpartcount<=count($stack)){
-			$res = $res.'</'.array_pop($stack).'>';
-		}
 		$type = str_replace(['head','list','item','lg','l'],['h'.$newurnpartcount,'ul','li','ul','li'],$row['type']);
 		$res = $res.'<'.$type.'>';
-		array_push($stack,$type);
 		$psgpart = $row['text'];
 		$psgpart = preg_replace('/<lb[^>]+>/', $nl, $psgpart);
 		$res = $res.preg_replace('/<[^>]+>/', '', $psgpart).$nl;
+		$res = $res.'</'.$type.'>';
 	}
-
-
 	return $res;
 }
 
 
-function passage($urn){
-	global $sql;
-	global $binary;
-	
-	if(str_ends_with($urn,':')){
-		$stmt = $sql->prepare('SELECT text,type,urn FROM urndata WHERE urn LIKE '.$binary.' ? ORDER BY urnid LIMIT 1,18446744073709551615');
-		$stmt->execute([$urn.'%']);
-	}
-	else {
-		#LIKE urn.% OR (exactly) LIKE urn
-		$stmt = $sql->prepare('SELECT text,type,urn FROM urndata WHERE urn LIKE '.$binary.' ? OR urn LIKE '.$binary.' ? ORDER BY urnid');
-		$stmt->execute([$urn.'.%',$urn]);
-	}
-	
-	$stack = [];
-	$res = '';
-	$oldurnpartcount = 0;
-	$nl = "\n";
-	foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row){
-		$newurnpartcount = count(explode('.',explode(':',$row['urn'])[4]));
-		if ($newurnpartcount==1){
-			$res = $res.'<hr>';
-		}
-		
-		while ($newurnpartcount<=count($stack)){
-			$res = $res.'</'.array_pop($stack).'>';
-		}
-		$type = str_replace(['head','list','item','lg','l'],['h'.$newurnpartcount,'ul','li','ul','li'],$row['type']);
-		$res = $res.'<'.$type.'>';
-		array_push($stack,$type);
-		$psgpart = $row['text'];
-		$psgpart = preg_replace('/<lb[^>]+>/', $nl, $psgpart);
-		$res = $res.preg_replace('/<[^>]+>/', '', $psgpart).$nl;
-	}
-	$res = str_replace($nl,'<br>',$res);
-	return $res;
-}
-?>
+passage?>
+
