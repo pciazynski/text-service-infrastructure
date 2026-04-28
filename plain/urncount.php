a304aaa9bfefa486e2efec6c8d4ca634b70aabfb
JTiepmar <jochen.tiepmar@serbski-institut.de>
2026-04-28 11:38:25 +0200

    bf plain/urncount

--- plain/urncount.php
+++ plain/urncount.php
@@ -2,16 +2,24 @@
 header('Content-Type: text/plain');
 require('../config.php');
 
-# Retuens number of static URNs on text level. This includes edition level URNs
+# Returns number of static URNs on text level. This includes edition level URNs
 
 function urncount(){
 	global $sql;
 	$query = '';
 	$res = '';
-	$stmt = $sql->prepare('SELECT MAX(maxi) as c FROM (SELECT MAX(urnid) AS maxi FROM urndata UNION SELECT MAX(urnid) AS maxi FROM urndatarestr) a');
+	$stmt = $sql->prepare('SELECT count(urn) as c FROM urndata');
 	$stmt->execute();
 	foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row){
-		$res = $row['c'];
+		$res = intval($row['c']);
+	}
+	
+	if($restricteddocuments){
+		$stmt = $sql->prepare('SELECT count(urn) as c FROM urndatarestr');
+		$stmt->execute();
+		foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row){
+			$res = $res + intval($row['c']);
+		}
 	}
 	return $res;
 }
