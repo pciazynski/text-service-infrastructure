7291db770a5b2b0920454ea5c7adc3222585b7ce
Jochen <xkf50md38vpm40heo0n20xjywyo2qw@bots.bitbucket.org>
2026-07-06 16:39:02 +0200

    createcache.py bugfix missing lang

--- createcache.py
+++ createcache.py
@@ -19,56 +19,61 @@ cs = mydb.cursor()
 cs.execute("SELECT urn,title,year,author,cast(restricted as CHAR(1)),lang FROM "+thisdb+".workdata ORDER BY author,title,urn")
 resstr=""
 for x in cs:
-    line = ""
-    for item in x:
-        if item == None:
-            item = ""
-    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])
-    resstr += line+"\n"
+    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])+"\n"
+    while "\tNone\t" in line:
+        line = line.replace("\tNone\t","\t\t")        
+    while "\tNone\n" in line:
+        line = line.replace("\tNone\n","\t\n")        
+    resstr += line
 
+    
 with open("plain/editions.cache", "w", encoding="utf8") as outf:
     outf.write(resstr.strip().replace("\tNone\t","\t\t"))
 
 cs.execute("SELECT urn,title,year,author,cast(restricted as CHAR(1)),lang FROM "+thisdb+".workdata ORDER BY author")
 resstr=""
 for x in cs:
-    for item in x:
-        if item == None:
-            item = ""
-    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])
-    resstr += line+"\n"
+    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])+"\n"
+    while "\tNone\t" in line:
+        line = line.replace("\tNone\t","\t\t")        
+    while "\tNone\n" in line:
+        line = line.replace("\tNone\n","\t\n")        
+    resstr += line
 with open("plain/editions_author.cache", "w", encoding="utf8") as outf:
     outf.write(resstr.strip().replace("\tNone\t","\t\t"))
     
 cs.execute("SELECT urn,title,year,author,cast(restricted as CHAR(1)),lang FROM "+thisdb+".workdata ORDER BY year")
 resstr=""
 for x in cs:
-    for item in x:
-        if item == None:
-            item = ""
-    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])
-    resstr += line+"\n"
+    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])+"\n"
+    while "\tNone\t" in line:
+        line = line.replace("\tNone\t","\t\t")        
+    while "\tNone\n" in line:
+        line = line.replace("\tNone\n","\t\n")        
+    resstr += line
 with open("plain/editions_year.cache", "w", encoding="utf8") as outf:
     outf.write(resstr.strip().replace("\tNone\t","\t\t"))
 
 cs.execute("SELECT urn,title,year,author,cast(restricted as CHAR(1)),lang FROM "+thisdb+".workdata ORDER BY author,year")
 resstr=""
 for x in cs:
-    for item in x:
-        if item == None:
-            item = ""
-    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])
-    resstr += line+"\n"
+    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])+"\n"
+    while "\tNone\t" in line:
+        line = line.replace("\tNone\t","\t\t")        
+    while "\tNone\n" in line:
+        line = line.replace("\tNone\n","\t\n")        
+    resstr += line
 with open("plain/editions_author,year.cache", "w", encoding="utf8") as outf:
     outf.write(resstr.strip().replace("\tNone\t","\t\t"))
 
 cs.execute("SELECT urn,title,year,author,cast(restricted as CHAR(1)),lang FROM "+thisdb+".workdata ORDER BY author,title")
 resstr=""
 for x in cs:
-    for item in x:
-        if item == None:
-            item = ""
-    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])
-    resstr += line+"\n"
+    line = str(x[0])+"\t"+str(x[1])+"\t"+str(x[2])+"\t"+str(x[3])+"\t"+str(x[4])+"\t"+str(x[5])+"\n"
+    while "\tNone\t" in line:
+        line = line.replace("\tNone\t","\t\t")        
+    while "\tNone\n" in line:
+        line = line.replace("\tNone\n","\t\n")        
+    resstr += line
 with open("plain/editions_author,title.cache", "w", encoding="utf8") as outf:
     outf.write(resstr.strip().replace("\tNone\t","\t\t"))
